115151133252b6cca2db751251873e2a80191c8d
Yvonne Su <articulation7@gmail.com>
2013-12-07 14:57:53 +0800

    style.css update

--- project3/js/script.js
+++ project3/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-	function randomColor(){
-		return "#" + Math.random().toString(16).slice(2,8);
-	};
+        function randomColor(){
+                return "#" + Math.random().toString(16).slice(2,8);
+        };
 
-	$("#button").on("click", function(){
-		$("body").css("background", randomColor());
-	});
+        $("#button").on("click", function(){
+                $("body").css("background", randomColor());
+        });
 });
